1b5a6c741d153429132311515cac872749cec425
MadakaHeri <madakaheri@icloud.com>
2025-09-08 00:51:32 +0900

    調整

--- .aws-sam/build/ServiceFunction/src/actions/hoge/index.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * A simple ping action that responds with a pong message.
- * @param {string} text - The parameters for the ping action.
- * @returns {Promise<string>} A pong message including the person's name and the text message.
- */
-export async function hoge(text) {
-	return `Hello, ${text} is hoge!`;
-}
